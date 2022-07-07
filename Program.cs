d08617711cdc6922ffb1482650cc9a9c80a2661d
Youan22 <you18034@byui.edu>
2022-07-07 11:59:20 -0600

    gh

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Unit04.Game.Casting;
+using Unit04.Game.Directing;
+using Unit04.Game.Services;
+
+
+namespace Unit04
+{
+    /// <summary>
+    /// The program's entry point.
+    /// </summary>
+    class Program
+    {
+        private static int FRAME_RATE = 24;
+        private static int MAX_X = 900;
+        private static int MAX_Y = 600;
+        private static int CELL_SIZE = 30;
+        private static int FONT_SIZE = 30;
+        private static int COLS = 60;
+        private static int ROWS = 40;
+
+        private static string CAPTION = "Greed";
+       
+        private static Color WHITE = new Color(255, 255, 255);
+      
+
+        /// <summary>
+        /// Starts the program using the given arguments.
+        /// </summary>
+        /// <param name="args">The given arguments.</param>
+        static void Main(string[] args)
+        {
+            // create the cast
+            Cast cast = new Cast();
+            Score score = new Score();
+            Random random = new Random();
+            
+            // create the player
+            Player player = new Player(FONT_SIZE, WHITE);
+            cast.AddActor("player", player);
+
+             // create the banner
+            Actor banner = new Actor();
+            banner.SetText($"Score: {score.getScore()}");
+            banner.SetFontSize(FONT_SIZE);
+            banner.SetColor(WHITE);
+            banner.SetPosition(new Point(CELL_SIZE, 0));
+            cast.AddActor("banner", banner);
+        
+                
+            // start the game
+            KeyboardService keyboardService = new KeyboardService(CELL_SIZE);
+            VideoService videoService 
+                = new VideoService(CAPTION, MAX_X, MAX_Y, CELL_SIZE, FRAME_RATE, false);
+            Director director = new Director(keyboardService, videoService);
+            director.StartGame(cast);
+
+            // test comment
+        }
+    }
+}
